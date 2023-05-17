780d26476651bacbd2f352e3325f9bb6d22a201a
Eduardo Gonzalez <eduardoglez0101100101@gmail.com>
2023-05-17 15:58:49 -0600

    Create components, add AOS n BS5-Icons, configure nav and footer

---
+++ src/components/body.vue
@@ -0,0 +1,62 @@
+<template>
+  <div data-bs-spy="scroll" data-bs-target="#nav-menu" data-bs-smooth-scroll="true" data-bs-root-margin="0px 0px -60%"
+    class="scrollspy-example bg-body-tertiary p-3 rounded-2 px-4" tabindex="0">
+    <h4 id="scrollspyHeading1">First heading</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto animi accusamus beatae impedit neque nemo
+      fugiat tenetur praesentium eius aliquid rem cumque nam eaque, voluptatum laborum! Fugit nulla beatae facere.
+      Saepe nesciunt, excepturi alias earum, voluptatibus sapiente aperiam, vitae omnis eveniet aut blanditiis doloribus
+      possimus dolorem quia rem esse. Nisi enim temporibus ea harum amet. Commodi doloremque dolorum minus doloribus!
+      Fugiat esse est temporibus facere distinctio sequi, et cumque aut excepturi error odit assumenda, in dolores
+      exercitationem dicta quaerat repudiandae eum aspernatur quis numquam, eius ex! Porro temporibus quas suscipit.
+      Iure nisi, quidem aliquid illum autem optio maiores quas accusantium doloribus. Labore rerum modi sit quis, nam
+      excepturi! Eligendi illo architecto corporis, voluptas ipsum quaerat neque libero vel culpa eum.
+      Ut iusto adipisci est atque sapiente corrupti nulla aliquam odit laudantium? Voluptates ratione quae cum tempora
+      dolorem enim atque, quos aliquam aperiam accusantium odit, asperiores expedita inventore. Dignissimos, dolores eius.
+    </p>
+    <br><br><br><br><br>
+    <h4 id="scrollspyHeading2">Second heading</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto animi accusamus beatae impedit neque nemo
+      fugiat tenetur praesentium eius aliquid rem cumque nam eaque, voluptatum laborum! Fugit nulla beatae facere.
+      Saepe nesciunt, excepturi alias earum, voluptatibus sapiente aperiam, vitae omnis eveniet aut blanditiis doloribus
+      possimus dolorem quia rem esse. Nisi enim temporibus ea harum amet. Commodi doloremque dolorum minus doloribus!
+      Fugiat esse est temporibus facere distinctio sequi, et cumque aut excepturi error odit assumenda, in dolores
+      exercitationem dicta quaerat repudiandae eum aspernatur quis numquam, eius ex! Porro temporibus quas suscipit.
+      Iure nisi, quidem aliquid illum autem optio maiores quas accusantium doloribus. Labore rerum modi sit quis, nam
+      excepturi! Eligendi illo architecto corporis, voluptas ipsum quaerat neque libero vel culpa eum.
+      Ut iusto adipisci est atque sapiente corrupti nulla aliquam odit laudantium? Voluptates ratione quae cum tempora
+      dolorem enim atque, quos aliquam aperiam accusantium odit, asperiores expedita inventore. Dignissimos, dolores eius.
+    </p>
+    <br><br><br><br><br>
+    <h4 id="scrollspyHeading3">Third heading</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto animi accusamus beatae impedit neque nemo
+      fugiat tenetur praesentium eius aliquid rem cumque nam eaque, voluptatum laborum! Fugit nulla beatae facere.
+      Saepe nesciunt, excepturi alias earum, voluptatibus sapiente aperiam, vitae omnis eveniet aut blanditiis doloribus
+      possimus dolorem quia rem esse. Nisi enim temporibus ea harum amet. Commodi doloremque dolorum minus doloribus!
+      Fugiat esse est temporibus facere distinctio sequi, et cumque aut excepturi error odit assumenda, in dolores
+      exercitationem dicta quaerat repudiandae eum aspernatur quis numquam, eius ex! Porro temporibus quas suscipit.
+      Iure nisi, quidem aliquid illum autem optio maiores quas accusantium doloribus. Labore rerum modi sit quis, nam
+      excepturi! Eligendi illo architecto corporis, voluptas ipsum quaerat neque libero vel culpa eum.
+      Ut iusto adipisci est atque sapiente corrupti nulla aliquam odit laudantium? Voluptates ratione quae cum tempora
+      dolorem enim atque, quos aliquam aperiam accusantium odit, asperiores expedita inventore. Dignissimos, dolores eius.
+    </p>
+    <br><br><br><br><br>
+    <h4 id="scrollspyHeading4">Fourth heading</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores praesentium id quam modi reprehenderit neque
+      expedita vero nemo! Cumque molestiae saepe incidunt veniam delectus autem reprehenderit unde at voluptatibus
+      assumenda?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid vitae et suscipit ipsum nam quaerat consequatur
+      obcaecati voluptates in iste? Voluptas ipsam at sunt sit quae, fugit molestiae cupiditate a.
+      Nulla laboriosam soluta cupiditate non. Itaque aliquam hic architecto praesentium ut aperiam doloremque officia,
+      voluptate exercitationem unde mollitia ea reprehenderit consectetur quis eius error doloribus adipisci.
+      Reprehenderit ut perferendis hic.
+    </p>
+  </div>
+</template>
+
+<script>
+export default{
+  mounted() {
+    this.AOS.init()
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import AOS from 'aos'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import './styles/styles.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(AOS)
+app.mount('#app')
